5805f7e999a4ca008400bbee1a09ca694e49302a
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-06-10 16:17:40 +0200

    refactor: Minor changes for learning purposes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,71 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { useState } from 'react'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<section id="center">
+				<div>
+					<h1>Sofus Lind</h1>
+					<p>
+						Work in progress homepage for public relations and personal use.
+					</p>
+				</div>
+				<button
+					type="button"
+					className="counter"
+					onClick={() => setCount((count) => count + 1)}
+				>
+					Count is {count}
+				</button>
+			</section>
+
+			<div className="ticks"></div>
+
+			<section id="next-steps">
+				<div id="docs">
+					<h2>Github links</h2>
+					<p>Personal github account and the AbaCord github organisation</p>
+					<ul>
+						<li>
+							<a href="https://github.com/sofuslind/" target="_blank">
+
+								Sofus Lind
+							</a>
+						</li>
+						<li>
+							<a href="https://github.com/AbaCord/" target="_blank">
+								AbaCord
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div id="social">
+					<h2>Other links</h2>
+					<p>Join AbaCord and visit the discord server</p>
+					<ul>
+						<li>
+							<a href="https://abakus.no/interest-groups/279" target="_blank">
+								Abakus - Abacord
+							</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+
+			<div className="ticks"></div>
+			<section id="spacer"></section>
+		</>
+	)
+}
+
+export default App
+
+ReactDOM.createRoot(
+	document.getElementById("root")!
+).render(
+	<App />
+);
