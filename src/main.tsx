2a493232e98b75bb5057e6023b1155f68d93ce45
Sofus Lind <sofushl@proton.me>
2026-07-08 18:27:09 +0200

    fix: Root element could be null error

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import App from './App';
 import './index.css';
 import { StrictMode } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const container = document.getElementById("root");
+
+if (!container) {
+    throw new Error("Root element not found");
+}
+
+const root = ReactDOM.createRoot(container);
+
 root.render(
     <StrictMode>
         <App />
